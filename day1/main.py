7fc1124f9e9fa36d6e01287513cc22e70b13514f
jkl46 <jay-klein@outlook.com>
2024-01-28 05:20:01 +0100

    rework with regex

--- day1/main.py
+++ day1/main.py
@@ -1,46 +1,34 @@
+import sys, re
 inp = [line.rstrip() for line in open("input.txt", "r")] 
 
 def part1():
-	nums = '1234567890'
-	totalSum = 0
-	for line in inp:
-		for x in range(len(line)):
-			if line[x] in nums:
-				a = line[x]
-				break
+	result = 0
+	numsREG = re.compile(r'\d')
 
-		for x in range(len(line)-1, -1, -1): 
-			if line[x] in nums:
-				b = line[x]
-				break
-		totalSum += int(a + b)
-	return totalSum
+	for line in inp:
+		nums = numsREG.findall(line)
+		result += int(nums[0] + nums[-1])
+
+	return result
 
 def part2():
-	nums = "1234567890"
-	numsInWord = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-	totalSum = 0
+	result = 0
 
-	def findFirstChar(line, reverse=False): # better to use function to break out of multiple for loops using return
-		for idx, char in enumerate(line):
-			if char in nums: # if character is 1 or 2 or 3, etc
-				return char
 
-			else: # if character maybe is part of "one", "two", "three"
-				for thisIdx, number in enumerate(numsInWord): 
-					if reverse:
-						number = number[::-1]
+	nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', *[str(x) for x in range(10)]]
+	numToInt = {key: x % 10 for key, x in zip(nums, range(20))} 
 
-					if line[idx:idx+len(number)] == number:
-						return str(thisIdx) 
-
-		raise Exception("not good")
+	getNums = re.compile(f'{"|".join(nums)}')
+	getReversedNums = re.compile(f'{"|".join(list(map(lambda x: x[::-1], nums)))}')
+	print(getReversedNums)
 
 	for line in inp:
-		a = findFirstChar(line)
-		b = findFirstChar(line[::-1], reverse=True) 
-		totalSum += int(a + b)
-	return totalSum
+		firstNum = numToInt[getNums.search(line).group()]
+		lastNum = numToInt[getReversedNums.search(line[::-1]).group()[::-1]]
+		result += int(str(firstNum) + str(lastNum))
+
+	return result
+
 
 print(f"awnser to part 1: {part1()}")
 print(f"awnser to part 2: {part2()}")
